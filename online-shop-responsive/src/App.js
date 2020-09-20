503d7f5bf06058c49a867a4227075bb6c8fdccf1
hoale240803 <hoale240803@gmail.com>
2020-09-20 13:30:46 +0700

    sua lan 1

--- online-shop-responsive/src/App.js
+++ online-shop-responsive/src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
+//t sua master roi nha
 function App() {
   return (
     <div className="App">
